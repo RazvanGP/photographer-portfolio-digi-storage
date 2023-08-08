3b7935f216d2b9f35d1646e636d7e4e19ff0231f
Razvan Plingu <plingu.razvan@gmail.com>
2023-08-08 18:22:03 +0300

    get auth token updated

--- app/events/page.js
+++ app/events/page.js
@@ -1,18 +1,20 @@
 "use client";
 
-import { connectToStorage } from "@/utils/storage";
+import { useState } from "react";
 
 const EventsPage = () => {
+  const [token, setToken] = useState();
+
+  const getAuthToken = async () => {
+    const res = await fetch("http://localhost:3000/api/get-auth-token");
+    const data = await res.json();
+    setToken(data.token);
+  };
+
   return (
     <main className="h-screen flex justify-center items-center">
       <h1>Events Page</h1>
-      <button
-        onClick={() => {
-          connectToStorage();
-        }}
-      >
-        Get auth token
-      </button>
+      <button onClick={() => getAuthToken()}>Get auth token</button>
     </main>
   );
 };
